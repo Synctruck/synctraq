32b73c8cce60599701516b7f1e120464185b7d68
Daniel Barrientos <danielbarrientos0802@gmail.com>
2022-08-25 21:50:01 -0500

    Modificando validaciones en driverController

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -96,6 +96,7 @@ class DriverController extends Controller
                 "nameOfOwner.max"  => "Debe ingresar máximo 150 dígitos",
 
                 "phone.required" => "El campo es requerido",
+                "phone.unique" => "El teléfono ya existe",
 
                 "email.unique" => "El correo ya existe",
                 "email.required" => "El campo es requerido",
@@ -176,6 +177,7 @@ class DriverController extends Controller
                 "address.required" => "El campo es requerido",
 
                 "phone.required" => "El campo es requerido",
+                "phone.unique" => "El teléfono ya existe",
 
                 "email.unique" => "El correo ya existe",
                 "email.required" => "El campo es requerido",
