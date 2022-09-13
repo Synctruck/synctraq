0f43433801d76cace548e869953c16332a8740c8
Wil CM <wilber.cahuana@synctruck.com>
2022-09-13 12:17:47 -0500

    ..

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -339,11 +339,11 @@
                             </a>
                         </li>
                         @endif
-                        @if(hasPermission('uncheckDelivery.index'))
+                        @if(hasPermission('validatedDelivery.index'))
                             <li>
                                 <a class="nav-link {{Request::is('package-delivery/finance') ? 'active' : 'collapsed'}}" href="{{url('package-delivery/finance')}}">
                                     <i class="bx bxs-dollar-circle"></i>
-                                    <span>UNCHECK DELIVERY</span>
+                                    <span>VALIDATE DELIVERY</span>
                                 </a>
                             </li>
                         @endif
