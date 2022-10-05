7ae85f27d15fafcd9bb43d1f213d6c24355597ef
Daniel Barrientos <danielbarrientos0802@gmail.com>
2022-10-04 22:50:57 -0500

    modificando track agregando steps

--- resources/js/components/Track.jsx
+++ resources/js/components/Track.jsx
@@ -4,18 +4,24 @@ import { Modal } from 'react'
 import Pagination from "react-js-pagination"
 import swal from 'sweetalert'
 import axios from 'axios';
-import moment from 'moment'
+import moment from 'moment';
+import { Steps } from 'rsuite';
+import '../../css/rsuit.css';
 
 function Track() {
 
     const [packageId, setPackageId] = useState('');
     const [listDetails, setListDetails] = useState([]);
+    const [step, setStep] = useState(null);
+
+    const [onholdDesc, setOnholdDesc] = useState('');
+    const [inboundDesc, setInboundDesc] = useState('');
+    const [dispatchDesc, setDispatchDesc] = useState('');
+    const [deliveryDesc, setDeliveryDesc] = useState('');
 
     useEffect(() => {
-
-        // listAllUser(page);
-
-    }, [])
+        handleStep();
+    }, [listDetails])
 
 
 
@@ -33,12 +39,82 @@ function Track() {
         .then((response) => {
             console.log(response.data);
             setListDetails(response.data.details);
+
         })
         .catch(function(error) {
            alert('Error:',error);
         })
         .finally();
     }
+
+    const handleStep =() => {
+        console.log('cambiando step');
+        let finalStep = null;
+        setOnholdDesc('');
+        setInboundDesc('');
+        setDeliveryDesc('');
+        setDispatchDesc('');
+
+        listDetails.map((item,i) => {
+            if(item.status == 'On hold'){
+                setOnholdDesc(moment(item.created_at).format('LL'))
+            }
+            if(item.status == 'Inbound'){
+                setInboundDesc(moment(item.created_at).format('LL'))
+            }
+            if(item.status == 'Dispatch'){
+                setDispatchDesc(moment(item.created_at).format('LL'))
+            }
+            if(item.status == 'Delivery'){
+                setDeliveryDesc(moment(item.created_at).format('LL'))
+            }
+        });
+
+
+
+        finalStep = listDetails.find(item => {
+            return item.status =='Delivery'
+        });
+        if(! finalStep){
+            finalStep = listDetails.find(item => {
+                return item.status =='Dispatch'
+            });
+
+            if(! finalStep){
+                finalStep = listDetails.find(item => {
+                    return item.status =='Inbound'
+                });
+            }
+
+            if(! finalStep){
+                finalStep = listDetails.find(item => {
+                    return item.status =='On hold'
+                });
+            }
+        }
+
+        if(finalStep){
+            console.log('final step: ',finalStep.status);
+
+            switch (finalStep.status) {
+                case 'On hold':
+                    setStep(0);
+                    break;
+                case 'Inbound':
+                    setStep(1);
+                    break;
+                case 'Dispatch':
+                    setStep(2);
+                    break;
+                case 'Delivery':
+                    setStep(3);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
     const detailsListTable = listDetails.map( (item, i) => {
 
         return (
@@ -50,6 +126,8 @@ function Track() {
         );
     });
 
+
+
     return (
 
         <section className="section">
@@ -57,7 +135,7 @@ function Track() {
                 <div className="card-body">
                     <div className=" pb-2">
                         <h5 className="card-title text-center pb-0 fs-4">Order tracking</h5>
-                        <p className="text-center small">NOTE: Package ID is the entire package identifier under the barcode on your package. Package ID Example: 222668400492</p>
+                        <p className="text-center"><span> NOTE: Package ID is the entire package identifier under the barcode on your package. Package ID Example: 222668400492 </span></p><br></br>
                         <div className="col-lg-12">
                             <form onSubmit={getDetail}>
                                 <div className="form-group">
@@ -74,8 +152,21 @@ function Track() {
                                 </div>
                             </form>
                         </div>
-                        <h6 className="pt-4">Traking details </h6><hr />traking details
-                        <div className="col-lg-6">
+
+
+                        <h6 className="pt-4">Traking details </h6><hr />
+                        <div className='row'>
+                             <h5 className='text-center'>PACKAGE ID: {packageId} </h5>
+                            <div className='col-12 mt-2'>
+                                <Steps current={step}>
+                                    <Steps.Item title="On hold" description={onholdDesc} />
+                                    <Steps.Item title="Inbound" description={inboundDesc}/>
+                                    <Steps.Item title="Dispatch" description={dispatchDesc}/>
+                                    <Steps.Item title="Delivery" description={deliveryDesc}/>
+                                </Steps>
+                            </div>
+                        </div>
+                        {/* <div className="col-lg-6">
 
                             <table className='table'>
                                 <thead>
@@ -88,7 +179,7 @@ function Track() {
                                     { detailsListTable }
                                 </tbody>
                             </table>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
