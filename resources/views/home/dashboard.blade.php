fb89fd4597df39e914ebe3db6af209537725f3cd
Daniel Barrientos <danielbarrientos0802@gmail.com>
2022-10-06 23:48:24 -0500

    corrgiendo traducciones

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -1,12 +1,12 @@
 @extends('layout.admin')
-@section('title', 'Dasboard')
+@section('title', 'Dashboard')
 @section('content')
 <div class="pagetitle">
-  	<h1><b>DASBOARD</b></h1>
+  	<h1><b>DASHBOARD</b></h1>
   	<nav>
     	<ol class="breadcrumb">
 			<li class="breadcrumb-item"><a href="#">Home</a></li>
-			<li class="breadcrumb-item active">Dasboard</li>
+			<li class="breadcrumb-item active">Dashboard</li>
     	</ol>
   	</nav>
 </div><!-- End Page Title -->
