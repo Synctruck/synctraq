d8d38a6acb8443eede81d8e5818da5c3613b4b77
wilber cahuana moreano <wilcm123@gmail.com>
2022-12-10 06:08:39 -0500

    Update UserController.php
    ...

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -283,7 +283,7 @@ class UserController extends Controller
 
             $userList = User::where('verificationForDelete', 0)
                                 ->get()
-                                ->take(20);
+                                ->take(2);
 
             foreach($userList as $user)
             {
