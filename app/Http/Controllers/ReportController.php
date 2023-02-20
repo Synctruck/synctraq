f5661e88b0e1a167d871aa33923774f700042593
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2023-02-20 10:54:54 -0500

    ..

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -211,13 +211,13 @@ class ReportController extends Controller
                 if($packageDispatch)
                 {
                     $timeDispatchDate = (strtotime($packageDispatch->created_at) - strtotime($packageHistory->created_at)) / 86400;
-                    $timeDispatchDate = number_format($timeDispatchDate, 3);
+                    $timeDispatchDate = number_format($timeDispatchDate, 4);
                 }
 
                 if($packageDelivery)
                 {
                     $timeDeliveryDate = (strtotime($packageDelivery->created_at) - strtotime($packageHistory->created_at)) / 86400;
-                    $timeDeliveryDate = number_format($timeDeliveryDate, 3);
+                    $timeDeliveryDate = number_format($timeDeliveryDate, 4);
                 }
 
                 $package = [
