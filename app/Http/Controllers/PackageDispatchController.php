fc46aae733810e7c3782e387883f0e291996c0a6
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2023-03-31 12:14:49 -0500

    ...

--- app/Http/Controllers/PackageDispatchController.php
+++ app/Http/Controllers/PackageDispatchController.php
@@ -543,6 +543,9 @@ class PackageDispatchController extends Controller
                             {
                                 DB::commit();
 
+                                $package['latitude']  = $request->get('latitude');
+                                $package['longitude'] = $request->get('longitude');
+
                                 //data for INLAND
                                 $packageController = new PackageController();
                                 $packageController->SendStatusToInland($package, 'Dispatch', null, $created_at);
