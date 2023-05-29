3466fc132a162f20cc602495a60b91c33502c947
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2023-05-29 11:22:19 -0500

    Update ExternalServiceInland.php

--- app/External/ExternalServiceInland.php
+++ app/External/ExternalServiceInland.php
@@ -12,25 +12,28 @@ class ExternalServiceInland{
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://api.staging.inlandlogistics.co/api/v6/shipments/'. $request->Reference_Number_1 .'/update-details',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'PUT',
-            CURLOPT_POSTFIELDS =>'{
-                "address_line1": "'. $request->Dropoff_Address_Line_1 .'",
-                "city_locality": "'. $request->Dropoff_City .'",
-                "state_province": "'. $request->Dropoff_Province .'",
-                "postal_code": "'. $request->Dropoff_Postal_Code .'",
-            }',
-            CURLOPT_HTTPHEADER => array(
-                'Content-Type: application/json',
-                'authorization: SHZX2ER-4YCM907-MM958YS-11GT162'
-            ),
+          CURLOPT_URL => 'https://api.staging.inlandlogistics.co/api/v6/shipments/84670689577/update-details',
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'PUT',
+          CURLOPT_POSTFIELDS =>'{
+            "address_line1": "address test",
+            "address_line2": "",
+            "city_locality": "new city",
+            "state_province": "new state",
+            "postal_code": "17321"
+        }',
+          CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json',
+            'authorization: SHZX2ER-4YCM907-MM958YS-11GT162'
+          ),
         ));
+
+        
 
         $output      = json_decode(curl_exec($curl), 1);
         $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
