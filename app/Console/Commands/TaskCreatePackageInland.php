4b92c1695f280df365c1391679c3021d79fb9dac
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2023-06-05 16:05:50 -0500

    ...

--- app/Console/Commands/TaskCreatePackageInland.php
+++ app/Console/Commands/TaskCreatePackageInland.php
@@ -56,7 +56,7 @@ class TaskCreatePackageInland extends Command
             $packageManifestList = PackageManifest::where('company', '!=', 'INLAND LOGISTICS')
                                             ->where('sendToInland', 0)
                                             ->get()
-                                            ->take(100);
+                                            ->take(10);
 
             foreach($packageManifestList as $packageManifest)
             {
