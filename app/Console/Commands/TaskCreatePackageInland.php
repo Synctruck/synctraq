9c9679d8b4a09855e8b564af16e52d965cec3ce2
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2023-06-07 11:28:00 -0500

    Update TaskCreatePackageInland.php

--- app/Console/Commands/TaskCreatePackageInland.php
+++ app/Console/Commands/TaskCreatePackageInland.php
@@ -139,8 +139,6 @@ class TaskCreatePackageInland extends Command
                 $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
                 curl_close($curl);
-
-                Log::info($data);
                 
                 if($http_status >= 200 && $http_status <= 299)
                 {
