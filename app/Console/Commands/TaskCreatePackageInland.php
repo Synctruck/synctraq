b17d166d28a3eabbadeaca005023a43e89d2fefc
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2023-06-12 16:09:19 -0500

    Update TaskCreatePackageInland.php

--- app/Console/Commands/TaskCreatePackageInland.php
+++ app/Console/Commands/TaskCreatePackageInland.php
@@ -145,8 +145,7 @@ class TaskCreatePackageInland extends Command
                 curl_close($curl);
                 
                 Log::info(ENV('URL_INLAND_CREATE') .'api/v6/add-to-manifest');
-                Log::info($data);
-                Log::info($http_status);
+                Log::info('http_status: '. $http_status);
 
                 if($http_status >= 200 && $http_status <= 299)
                 {
