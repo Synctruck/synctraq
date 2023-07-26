4eb740ff16ed91c4f401476039421f4a83e93c17
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2023-07-26 05:15:46 -0500

    Update PackageDeliveryController.php

--- app/Http/Controllers/PackageDeliveryController.php
+++ app/Http/Controllers/PackageDeliveryController.php
@@ -428,8 +428,8 @@ class PackageDeliveryController extends Controller
 
     public function ListInvoiced()
     {
-        $dateInit = '2023-04-01 00:00:00';
-        $dateEnd  = '2023-04-30 23:59:59';
+        $dateInit = '2023-05-01 00:00:00';
+        $dateEnd  = '2023-05-31 23:59:59';
 
         $listAll = PackageDispatch::whereBetween('Date_Delivery', [$dateInit, $dateEnd])->get('Reference_Number_1');
 
