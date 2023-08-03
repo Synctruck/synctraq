8e96362e61e2ec83a2f810afce81071d19a70eb2
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2023-08-02 20:26:05 -0500

    refactor register driver

--- resources/js/components/Driver.jsx
+++ resources/js/components/Driver.jsx
@@ -520,7 +520,7 @@ function Driver() {
                                                                     <div className="col-lg-12">
                                                                         <div className="form-group">
                                                                             <label htmlFor="">TEAM</label>
-                                                                            <select name="" id="" className="form-control" onChange={ (e) => setIdTeam(e.target.value) }>
+                                                                            <select name="" id="" className="form-control" onChange={ (e) => setIdTeam(e.target.value) } required>
                                                                                 <option value="" style={ {display: 'none'} }>Select a team</option>
                                                                                 { listTeamSelect }
                                                                             </select>
