a778b474fc693c5aa1a6c1a229f8a4347104c7d8
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2023-09-18 09:20:52 -0500

    Update TaskPackageSendPreFactura.php

--- app/Console/Commands/TaskPackageSendPreFactura.php
+++ app/Console/Commands/TaskPackageSendPreFactura.php
@@ -85,7 +85,7 @@ class TaskPackageSendPreFactura extends Command
             DB::rollback(); 
         }*/
 
-        if($dayName == 'Monday' && $nowHour == 9)
+        if($dayName == 'Monday')
         {
             try
             {
