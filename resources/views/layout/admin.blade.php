85fa3aeefac93f51bc57f5c2f8480fe2c8726923
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2023-09-28 08:05:21 -0500

    Update admin.blade.php

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -881,7 +881,6 @@
 
                     if(package.status == 'Dispatch')
                     {
-                        alert(2);
                         Description_Return = `<br><b class="text-warning">Warehouse (${nameCellar}):  ${cityCellar}, ${stateCellar}</b>`;
                         Description = package.Description;
                     }
