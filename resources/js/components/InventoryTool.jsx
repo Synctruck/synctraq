ccd023ca00b3a4d9c4cb438318ae4be5d3db4765
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2023-10-09 10:26:22 -0500

    ...

--- resources/js/components/InventoryTool.jsx
+++ resources/js/components/InventoryTool.jsx
@@ -370,7 +370,7 @@ function InventoryTool() {
                                                                 <table className="table table-hover table-condensed table-bordered">
                                                                     <thead>
                                                                         <tr>
-                                                                            <th>PENDING ({ listInventoryToolDetailPending.length })</th>
+                                                                            <th>PENDING WAREHOUSE ({ listInventoryToolDetailPending.length })</th>
                                                                         </tr>
                                                                     </thead>
                                                                     <tbody>
