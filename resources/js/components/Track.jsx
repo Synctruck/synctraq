67f6f0286a6f01ad020d2b292db20140a5a7637a
Alvarogranillo61 <alvaro.granillo@inlandlogistics.co>
2023-10-16 15:52:17 -0600

    .

--- resources/js/components/Track.jsx
+++ resources/js/components/Track.jsx
@@ -1,89 +1,76 @@
-import React, { useState, useEffect } from 'react'
-import ReactDOM from 'react-dom'
-import { Modal } from 'react'
-import Pagination from "react-js-pagination"
-import swal from 'sweetalert'
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import Pagination from "react-js-pagination";
 import axios from 'axios';
 import moment from 'moment';
 import { Steps } from 'rsuite';
 import '../../css/rsuit.css';
 
 function Track() {
-
-    const [packageId, setPackageId]         = useState('');
+    const [packageId, setPackageId] = useState('');
     const [packageZipCode, setPackageZipCode] = useState('');
-
     const [listDetails, setListDetails] = useState([]);
     const [step, setStep] = useState(null);
-
     const [onholdDesc, setOnholdDesc] = useState('');
     const [inboundDesc, setInboundDesc] = useState('');
     const [dispatchDesc, setDispatchDesc] = useState('');
     const [deliveryDesc, setDeliveryDesc] = useState('');
+    const [searchClicked, setSearchClicked] = useState(false); // Variable para rastrear si se hizo clic en Search
 
     useEffect(() => {
         handleStep();
-    }, [listDetails])
+    }, [listDetails]);
 
-    useEffect( () => {
-
-        if(textSearch != '')
-        {
-            setPackageId(textSearch);
-
-            history.pushState(null, "", "track-detail?textSearch="+ textSearch);
+    useEffect(() => {
+        if (packageId !== '' && searchClicked) { // Verificar si se ingresó un Package ID y se hizo clic en Search
+            history.pushState(null, "", "track-detail?textSearch=" + packageId);
 
             console.log('submit');
 
-            let url    = url_general +'track/detail/'+ textSearch;
-            let method = 'GET'
+            let url = url_general + 'track/detail/' + packageId;
+            let method = 'GET';
 
             axios({
                 method: method,
                 url: url
             })
             .then((response) => {
-
                 console.log(response.data);
                 setListDetails(response.data.details);
                 setPackageZipCode(response.data.details[0].Dropoff_Postal_Code);
             })
-            .catch(function(error) {
-               alert('Error:',error);
+            .catch(function (error) {
+                alert('Error:', error);
             })
             .finally();
         }
-
-    }, []);
+    }, [packageId, searchClicked]);
 
     const getDetail = (e) => {
-
-        history.pushState(null, "", "track-detail?textSearch="+ packageId);
-
         e.preventDefault();
+        setSearchClicked(true); // Marcar que se hizo clic en Search
 
         console.log('submit');
 
-        let url    = url_general +'track/detail/'+ packageId;
-        let method = 'GET'
+        let url = url_general + 'track/detail/' + packageId;
+        let method = 'GET';
 
         axios({
             method: method,
             url: url
         })
         .then((response) => {
-
             console.log(response.data);
             setListDetails(response.data.details);
             setPackageZipCode(response.data.details[0].Dropoff_Contact_Name);
         })
-        .catch(function(error) {
-           alert('Error:',error);
+        .catch(function (error) {
+            alert('Error:', error);
         })
         .finally();
     }
 
-    const handleStep =() => {
+    const handleStep = () => {
         console.log('cambiando step');
         let finalStep = null;
         setOnholdDesc('');
@@ -91,46 +78,44 @@ function Track() {
         setDeliveryDesc('');
         setDispatchDesc('');
 
-        listDetails.map((item,i) => {
-            if(item.status == 'Manifest'){
-                setOnholdDesc(moment(item.created_at).format('LL'))
+        listDetails.map((item, i) => {
+            if (item.status == 'Manifest') {
+                setOnholdDesc(moment(item.created_at).format('LL'));
             }
-            if(item.status == 'Inbound'){
-                setInboundDesc(moment(item.created_at).format('LL'))
+            if (item.status == 'Inbound') {
+                setInboundDesc(moment(item.created_at).format('LL'));
             }
-            if(item.status == 'Dispatch'){
-                setDispatchDesc(moment(item.created_at).format('LL'))
+            if (item.status == 'Dispatch') {
+                setDispatchDesc(moment(item.created_at).format('LL'));
             }
-            if(item.status == 'Delivery'){
-                setDeliveryDesc(moment(item.created_at).format('LL'))
+            if (item.status == 'Delivery') {
+                setDeliveryDesc(moment(item.created_at).format('LL'));
             }
         });
 
-
-
-        finalStep = listDetails.find(item => {
-            return item.status =='Delivery'
+        finalStep = listDetails.find((item) => {
+            return item.status == 'Delivery';
         });
-        if(! finalStep){
-            finalStep = listDetails.find(item => {
-                return item.status =='Dispatch'
+        if (!finalStep) {
+            finalStep = listDetails.find((item) => {
+                return item.status == 'Dispatch';
             });
 
-            if(! finalStep){
-                finalStep = listDetails.find(item => {
-                    return item.status =='Inbound'
+            if (!finalStep) {
+                finalStep = listDetails.find((item) => {
+                    return item.status == 'Inbound';
                 });
             }
 
-            if(! finalStep){
-                finalStep = listDetails.find(item => {
-                    return item.status =='Manifest'
+            if (!finalStep) {
+                finalStep = listDetails.find((item) => {
+                    return item.status == 'Manifest';
                 });
             }
         }
 
-        if(finalStep){
-            console.log('final step: ',finalStep.status);
+        if (finalStep) {
+            console.log('final step: ', finalStep.status);
 
             switch (finalStep.status) {
                 case 'Manifest':
@@ -151,54 +136,54 @@ function Track() {
         }
     }
 
-    const detailsListTable = listDetails.map( (item, i) => {
-
+    const detailsListTable = listDetails.map((item, i) => {
         return (
-
             <tr key={i}>
-                <td>{ moment(item.created_at).format('LLLL') }</td>
-                <td>{ item.status }</td>
+                <td>{moment(item.created_at).format('LLLL')}</td>
+                <td>{item.status}</td>
             </tr>
         );
     });
 
-
-
     return (
-
         <section className="section">
-                    
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <form id="formSearch" onSubmit={getDetail}>
-                                    <div className="form-group">
-                                        <input
-                                            type="text"
-                                            id="textSearch"
-                                            className="form-control"
-                                            placeholder="Package ID"
-                                            required
-                                            value={packageId}
-                                            onChange={(e) => setPackageId(e.target.value)}
-                                            /><br />
-                                        <button className='btn' type='submit' style={{ backgroundColor: '#015E7C', color: 'white' }}> Search</button>
-                                    </div>
-                                </form>
-                            </div>
+            <div className="row">
+                <div className="col-lg-12">
+                    <form id="formSearch" onSubmit={getDetail}>
+                        <div className="form-group">
+                            <input
+                                type="text"
+                                id="textSearch"
+                                className="form-control"
+                                placeholder="Package ID"
+                                required
+                                value={packageId}
+                                onChange={(e) => setPackageId(e.target.value)}
+                            />
+                            <br />
+                            <button className='btn' type='submit' style={{ backgroundColor: '#015E7C', color: 'white' }}>Search</button>
                         </div>
+                    </form>
+                </div>
+            </div>
 
-                        <h6 className="pt-4">Tracking details </h6><hr />
-                        <div className='row'>
-                            <h5 className='text-center'>PACKAGE ID: {packageId}  / DELIVERY ZIP CODE: { packageZipCode }</h5>
-                            <div className='col-12 mt-2'>
-                                <Steps current={step}>
-                                    <Steps.Item title="In Fulfillment" description={onholdDesc} />
-                                    <Steps.Item title="Inbound" description={inboundDesc}/>
-                                    <Steps.Item title="Out for Delivery" description={dispatchDesc}/>
-                                    <Steps.Item title="Delivery" description={deliveryDesc}/>
-                                </Steps>
-                            </div>
+            {searchClicked && (
+                <div>
+                    <h6 className="pt-4">Tracking details</h6>
+                    <hr />
+                    <div className='row'>
+                        <h5 className='text-center'>PACKAGE ID: {packageId}  / DELIVERY ZIP CODE: {packageZipCode}</h5>
+                        <div className='col-12 mt-2'>
+                            <Steps current={step}>
+                                <Steps.Item title="In Fulfillment" description={onholdDesc} />
+                                <Steps.Item title="Inbound" description={inboundDesc} />
+                                <Steps.Item title="Out for Delivery" description={dispatchDesc} />
+                                <Steps.Item title="Delivery" description={deliveryDesc} />
+                            </Steps>
                         </div>
+                    </div>
+                </div>
+            )}
         </section>
     );
 }
