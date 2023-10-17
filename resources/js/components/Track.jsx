ea14bee7f6c55e1b034a0442ef10f5e4b199920c
Alvarogranillo61 <alvaro.granillo@inlandlogistics.co>
2023-10-17 15:16:48 -0600

    .

--- resources/js/components/Track.jsx
+++ resources/js/components/Track.jsx
@@ -154,23 +154,24 @@ function Track() {
             </div>
 
             {searchClicked && !searchFieldChanged && listDetails.length > 0 && (
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12 d-none d-lg-block">
-                            <h6 className="pt-4">Tracking details</h6>
-                            <hr />
-                            <h5 className="text-center">PACKAGE ID: {packageId}  / OWNER: {packageZipCode}</h5>
-                            <div className={`col-12 mt-2 tracking-details`}>
-                                <Steps current={step}>
-                                    <Steps.Item title="In Fulfillment" />
-                                    <Steps.Item title="Inbound" />
-                                    <Steps.Item title="Out for Delivery"/>
-                                    <Steps.Item title="Delivery"/>
-                                </Steps>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+             <div className="container">
+             <div className="row">
+               <div className="col-lg-12 d-none d-lg-block">
+                 <h6 className="pt-4">Tracking details</h6>
+                 <hr />
+                 <h5 className="text-center">PACKAGE ID: {packageId}  / OWNER: {packageZipCode}</h5>
+                 <div className={`col-12 mt-2 tracking-details`}>
+                   <div className="vertical-progress-bar">
+                     {listDetails.map((item, index) => (
+                       <div key={index} className={`step ${index === step ? "current" : ""}`}>
+                         <p>{item.title}</p>
+                       </div>
+                     ))}
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
             )}
         </section>
     );
