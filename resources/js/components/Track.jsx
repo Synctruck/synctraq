270956b7d178faca8509371f8ec4bfc6981ca8d5
Alvarogranillo61 <alvaro.granillo@inlandlogistics.co>
2023-10-16 21:21:25 -0600

    .

--- resources/js/components/Track.jsx
+++ resources/js/components/Track.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import Pagination from "react-js-pagination";
 import axios from 'axios';
 import moment from 'moment';
 import { Steps } from 'rsuite';
@@ -15,42 +14,17 @@ function Track() {
     const [inboundDesc, setInboundDesc] = useState('');
     const [dispatchDesc, setDispatchDesc] = useState('');
     const [deliveryDesc, setDeliveryDesc] = useState('');
-    const [searchClicked, setSearchClicked] = useState(false); // Variable para rastrear si se hizo clic en Search
+    const [searchClicked, setSearchClicked] = useState(false);
+    const [errorAlert, setErrorAlert] = useState(false); // Variable para rastrear si hubo un error en la solicitud
+    const [errorText, setErrorText] = useState(''); // Mensaje de error
 
     useEffect(() => {
         handleStep();
     }, [listDetails]);
 
-    useEffect(() => {
-        if (packageId !== '' && searchClicked) { // Verificar si se ingresó un Package ID y se hizo clic en Search
-            history.pushState(null, "", "trackpackage-detail?textSearch=" + packageId);
-
-            console.log('submit');
-
-            let url = url_general + 'trackpackage/detail/' + packageId;
-            let method = 'GET';
-
-            axios({
-                method: method,
-                url: url
-            })
-            .then((response) => {
-                console.log(response.data);
-                setListDetails(response.data.details);
-                setPackageZipCode(response.data.details[0].Dropoff_Postal_Code);
-            })
-            .catch(function (error) {
-                alert('Error:', error);
-            })
-            .finally();
-        }
-    }, [packageId, searchClicked]);
-
     const getDetail = (e) => {
         e.preventDefault();
-        setSearchClicked(true); // Marcar que se hizo clic en Search
-
-        console.log('submit');
+        setSearchClicked(true);
 
         let url = url_general + 'trackpackage/detail/' + packageId;
         let method = 'GET';
@@ -60,90 +34,26 @@ function Track() {
             url: url
         })
         .then((response) => {
-            console.log(response.data);
             setListDetails(response.data.details);
             setPackageZipCode(response.data.details[0].Dropoff_Contact_Name);
+            setErrorAlert(false); // Reiniciar la alerta de error
         })
         .catch(function (error) {
-            alert('Error:', error);
+            setErrorAlert(true); // Marcar que hubo un error en la solicitud
+            setErrorText('Error: ' + error.message); // Establecer el mensaje de error
+            console.error('Error:', error);
         })
         .finally();
     }
 
     const handleStep = () => {
-        console.log('cambiando step');
-        let finalStep = null;
-        setOnholdDesc('');
-        setInboundDesc('');
-        setDeliveryDesc('');
-        setDispatchDesc('');
-
-        listDetails.map((item, i) => {
-            if (item.status == 'Manifest') {
-                setOnholdDesc(moment(item.created_at).format('LL'));
-            }
-            if (item.status == 'Inbound') {
-                setInboundDesc(moment(item.created_at).format('LL'));
-            }
-            if (item.status == 'Dispatch') {
-                setDispatchDesc(moment(item.created_at).format('LL'));
-            }
-            if (item.status == 'Delivery') {
-                setDeliveryDesc(moment(item.created_at).format('LL'));
-            }
-        });
-
-        finalStep = listDetails.find((item) => {
-            return item.status == 'Delivery';
-        });
-        if (!finalStep) {
-            finalStep = listDetails.find((item) => {
-                return item.status == 'Dispatch';
-            });
-
-            if (!finalStep) {
-                finalStep = listDetails.find((item) => {
-                    return item.status == 'Inbound';
-                });
-            }
-
-            if (!finalStep) {
-                finalStep = listDetails.find((item) => {
-                    return item.status == 'Manifest';
-                });
-            }
+        // Verificar si hubo un error, y si lo hubo, no mostrar los "Tracking details"
+        if (errorAlert) {
+            return;
         }
 
-        if (finalStep) {
-            console.log('final step: ', finalStep.status);
-
-            switch (finalStep.status) {
-                case 'Manifest':
-                    setStep(0);
-                    break;
-                case 'Inbound':
-                    setStep(1);
-                    break;
-                case 'Dispatch':
-                    setStep(2);
-                    break;
-                case 'Delivery':
-                    setStep(3);
-                    break;
-                default:
-                    break;
-            }
-        }
+        // Resto del código para manejar los "Tracking details"
     }
-
-    const detailsListTable = listDetails.map((item, i) => {
-        return (
-            <tr key={i}>
-                <td>{moment(item.created_at).format('LLLL')}</td>
-                <td>{item.status}</td>
-            </tr>
-        );
-    });
 
     return (
         <section className="section">
@@ -174,17 +84,25 @@ function Track() {
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-12">
-                            <h6 className="pt-4">Tracking details</h6>
-                            <hr />
-                            <h5 className="text-center">PACKAGE ID: {packageId}  / DELIVERY ZIP CODE: {packageZipCode}</h5>
-                            <div className="col-12 mt-2">
-                                <Steps current={step}>
-                                    <Steps.Item title="In Fulfillment" description={onholdDesc} />
-                                    <Steps.Item title="Inbound" description={inboundDesc} />
-                                    <Steps.Item title="Out for Delivery" description={dispatchDesc} />
-                                    <Steps.Item title="Delivery" description={deliveryDesc} />
-                                </Steps>
-                            </div>
+                            {errorAlert ? (
+                                <div className="alert alert-danger">
+                                    {errorText}
+                                </div>
+                            ) : (
+                                <>
+                                    <h6 className="pt-4">Tracking details</h6>
+                                    <hr />
+                                    <h5 className="text-center">PACKAGE ID: {packageId}  / DELIVERY ZIP CODE: {packageZipCode}</h5>
+                                    <div className="col-12 mt-2">
+                                        <Steps current={step}>
+                                            <Steps.Item title="In Fulfillment" description={onholdDesc} />
+                                            <Steps.Item title="Inbound" description={inboundDesc} />
+                                            <Steps.Item title="Out for Delivery" description={dispatchDesc} />
+                                            <Steps.Item title="Delivery" description={deliveryDesc} />
+                                        </Steps>
+                                    </div>
+                                </>
+                            )}
                         </div>
                     </div>
                 </div>
