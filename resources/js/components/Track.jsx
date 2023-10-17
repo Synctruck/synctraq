8a92b727de209d4ab7b53f9aa56ecf206c7efa14
Alvarogranillo61 <alvaro.granillo@inlandlogistics.co>
2023-10-17 14:50:59 -0600

    .

--- resources/js/components/Track.jsx
+++ resources/js/components/Track.jsx
@@ -154,24 +154,38 @@ function Track() {
             </div>
 
             {searchClicked && !searchFieldChanged && listDetails.length > 0 && (
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <h6 className="pt-4">Tracking details</h6>
-                            <hr />
-                            <h5 className="text-center">PACKAGE ID: {packageId}  / OWNER: {packageZipCode}</h5>
-                            <div className={`col-12 mt-2 tracking-details`}>
-                                <Steps current={step}>
-                                    <Steps.Item title="In Fulfillment" />
-                                    <Steps.Item title="Inbound" />
-                                    <Steps.Item title="Out for Delivery"/>
-                                    <Steps.Item title="Delivery"/>
-                                </Steps>
-                            </div>
-                        </div>
-                    </div>
+    <div className="container">
+        <div className="row">
+            <div className="col-lg-6">
+                <h6 className="pt-4">Tracking details (Row)</h6>
+                <hr />
+                <h5 className="text-center">PACKAGE ID: {packageId}  / OWNER: {packageZipCode}</h5>
+                <div className={`col-12 mt-2 tracking-details`}>
+                    <Steps current={step}>
+                        <Steps.Item title="In Fulfillment" />
+                        <Steps.Item title="Inbound" />
+                        <Steps.Item title="Out for Delivery"/>
+                        <Steps.Item title="Delivery"/>
+                    </Steps>
                 </div>
-            )}
+            </div>
+            <div className="col-lg-6">
+                <h6 className="pt-4">Tracking details (Column)</h6>
+                <hr />
+                <h5 className="text-center">PACKAGE ID: {packageId}  / OWNER: {packageZipCode}</h5>
+                <div className={`col-12 mt-2 tracking-details`}>
+                    <Steps current={step}>
+                        <Steps.Item title="In Fulfillment" />
+                        <Steps.Item title="Inbound" />
+                        <Steps.Item title="Out for Delivery"/>
+                        <Steps.Item title="Delivery"/>
+                    </Steps>
+                </div>
+            </div>
+        </div>
+    </div>
+)}
+
         </section>
     );
 
