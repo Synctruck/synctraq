c28848344e699fbd3be67e7b68ad83680cc1d2d8
Alvarogranillo61 <alvaro.granillo@inlandlogistics.co>
2023-10-17 23:28:07 -0600

    .

--- resources/js/components/Track.jsx
+++ resources/js/components/Track.jsx
@@ -175,7 +175,7 @@ function Track() {
             <div className="col-12 mt-2 tracking-details  d-block d-sm-none">
             <div className="row">
               <div className="col-md-3">
-              <Steps current={step === 0 ? 0 : 1} className="text-center">
+            <Steps current={step === 0 ? 0 : 1} className="text-center">
   <Steps.Item>
     {step === 0 ? (
       <Steps.Item title="In Fulfillment" />
@@ -187,7 +187,7 @@ function Track() {
 
              </div>
             <div className="col-md-3">
-              <Steps current={step === 1 ? 0 : 1} className="text-center">
+              <Steps current={step === 1 ? 0 : "2"} className="text-center">
                 <Steps.Item title="Inbound" />
               </Steps>
             </div>
