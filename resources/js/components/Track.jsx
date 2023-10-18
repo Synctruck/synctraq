12157fb8a4e9884e8b94e304e6b6aeced6a1b52c
Alvarogranillo61 <alvaro.granillo@inlandlogistics.co>
2023-10-17 21:07:24 -0600

    .

--- resources/js/components/Track.jsx
+++ resources/js/components/Track.jsx
@@ -156,26 +156,18 @@ function Track() {
             {searchClicked && !searchFieldChanged && listDetails.length > 0 && (
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-12">
+                        <div className="col-lg-12 d-none d-lg-block">
                             <h6 className="pt-4">Tracking details</h6>
                             <hr />
                             <h5 className="text-center">PACKAGE ID: {packageId}  / OWNER: {packageZipCode}</h5>
                             <div className={`col-12 mt-2 tracking-details`} style={{ flexDirection: 'column' }}>
-                            <Steps current={step}>
-                            <div className="col-lg-3 col-md-12 col-sm-12">
-                            <Steps.Item title="In Fulfillment" description={onholdDesc} />
-                            </div>
-                            <div className="col-lg-3 col-md-12 col-sm-12">
-                             <Steps.Item title="Inbound" description={inboundDesc} />
-                            </div>
-                            <div className="col-lg-3 col-md-12 col-sm-12">
-                            <Steps.Item title="Out for Delivery" description={dispatchDesc} />
-                            </div>
-                             <div className="col-lg-3 col-md-12 col-sm-12">
-                          <Steps.Item title="Delivery" description={deliveryDesc} />
-                         </div>
-                        </Steps>
-                        </div>
+    <Steps current={step}>
+        <Steps.Item title="In Fulfillment" />
+        <Steps.Item title="Inbound" />
+        <Steps.Item title="Out for Delivery" />
+        <Steps.Item title="Delivery" />
+    </Steps>
+</div>
 
                         </div>
                     </div>
