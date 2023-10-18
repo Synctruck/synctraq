0eb434e5b0e69663102e2131c1c9363440122808
Alvarogranillo61 <alvaro.granillo@inlandlogistics.co>
2023-10-18 11:43:15 -0600

    .

--- resources/js/components/packagetrack.jsx
+++ resources/js/components/packagetrack.jsx
@@ -173,7 +173,7 @@ function PackageTrack() {
                         <div className="row">
                             <div className="col-lg-12 text-center">
                                 <div className="form-group">
-                                    <img src="./img/logo_sync_truck.png" alt="" width="220"/>
+                                    <img src="./img/log.png" alt="" width="220"/>
                                 </div>
                             </div>
                             <div className="col-lg-12">
@@ -192,7 +192,7 @@ function PackageTrack() {
                                             value={packageId}
                                             onChange={(e) => setPackageId(e.target.value)}
                                             /><br />
-                                        <button className='btn' style={{ backgroundColor: '#015E7C', color: 'white' }} type='submit'>Search</button>
+                                        <button className='btn' type='submit' style={{ backgroundColor: '#015E7C', color: 'white' }}> Search</button>
                                     </div>
                                 </form>
                             </div>
