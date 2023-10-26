2494021b530726cfed1d4afe50c92d39e3369ea1
Alvarogranillo61 <alvaro.granillo@inlandlogistics.co>
2023-10-26 11:29:57 -0600

    .

--- resources/js/components/Track.jsx
+++ resources/js/components/Track.jsx
@@ -191,7 +191,7 @@ function Track() {
                 </div>
               </div>
           
-            {searchClicked && listDetails.length > 0 &&  !searchFieldChanged &&(
+            {listDetails.length > 0 &&  !searchFieldChanged &&(
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12">
