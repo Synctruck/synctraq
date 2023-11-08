75fe52705c78462c47ab3b0159094f9719da2d53
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2023-11-08 06:58:59 -0500

    Update TaskPaymentTeam.php

--- app/Console/Commands/TaskPaymentTeam.php
+++ app/Console/Commands/TaskPaymentTeam.php
@@ -321,9 +321,10 @@ class TaskPaymentTeam extends Command
             $timeChangeDateEnd   = strtotime(date('Y-m-d', strtotime($nowDate .' +6 day')));
             $timeDeliveryDate    = strtotime(date('Y-m-d', strtotime($Date_Delivery)));
 
-            Log::info($timeChangeDateStart .' <= '. $timeDeliveryDate.' && '. $timeDeliveryDate .' <= '. $timeChangeDateEnd);
             if($timeChangeDateStart <= $timeDeliveryDate && $timeDeliveryDate <= $timeChangeDateEnd)
             {
+                Log::info($timeChangeDateStart .' <= '. $timeDeliveryDate.' && '. $timeDeliveryDate .' <= '. $timeChangeDateEnd);
+                
                 $dieselPriceCompany = $historyDiesel->roundPrice;
             }
         }
