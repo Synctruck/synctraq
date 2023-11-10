40fff540417884ebeaebf794fe8363e12fc660a0
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2023-11-10 11:11:43 -0500

    Update PaymentTeamController.php

--- app/Http/Controllers/PaymentTeamController.php
+++ app/Http/Controllers/PaymentTeamController.php
@@ -90,7 +90,7 @@ class PaymentTeamController extends Controller
                 $paymentDetail = PaymentTeamDetail::where('Reference_Number_1', $paymentDetail->Reference_Number_1)->first();
                 $paymentDetail->Reference_Number_1  = $packageHistory->Reference_Number_1;
                 $paymentDetail->Route               = $packageHistory->Route;
-                $paymentDetail->idPaymentTeam       = $payment->idTeam;
+                $paymentDetail->idPaymentTeam       = $team->idTeam;
                 $paymentDetail->dimFactor           = $dimFactor;
                 $paymentDetail->save();
 
