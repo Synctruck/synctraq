3d7ba8dc04770a752cd79d356d8b788c3c652a5a
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2023-11-20 11:56:07 -0500

    Update PackageDispatchController.php

--- app/Http/Controllers/Api/PackageDispatchController.php
+++ app/Http/Controllers/Api/PackageDispatchController.php
@@ -231,7 +231,7 @@ class PackageDispatchController extends Controller
                             $packageHistory->updated_at                   = $created_at;
                             $packageHistory->save();
 
-                            if($package->status == 'Warehouse')
+                            if($package->status == 'Manifest' || $package->status == 'Warehouse')
                             {
                                 $package->delete();
                             }
