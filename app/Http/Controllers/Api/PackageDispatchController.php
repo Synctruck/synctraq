f2ca0e100057a1e47a1a4676672fabcf032c6062
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2023-11-29 10:41:55 -0500

    Update PackageDispatchController.php

--- app/Http/Controllers/Api/PackageDispatchController.php
+++ app/Http/Controllers/Api/PackageDispatchController.php
@@ -344,7 +344,7 @@ class PackageDispatchController extends Controller
                     $packageController = new PackageController();
 
                     if($packageDispatch->idCompany == 1)
-                        $packageController->SendStatusToInland($packageDispatch, 'Delivery', explode(',', $photoUrl), $created_at);
+                        $packageController->SendStatusToInland($packageDispatch, 'Delivery', explode(',', $photoUrl), date('Y-m-d H:i:s'));
 
                     $packageHistory = PackageHistory::where('Reference_Number_1', $packageDispatch->Reference_Number_1)
                                                 ->where('sendToInland', 1)
@@ -352,7 +352,7 @@ class PackageDispatchController extends Controller
                                                 ->first();
 
                     if($packageHistory)
-                        $packageController->SendStatusToOtherCompany($packageDispatch, 'Delivery', explode(',', $photoUrl), $created_at);
+                        $packageController->SendStatusToOtherCompany($packageDispatch, 'Delivery', explode(',', $photoUrl), date('Y-m-d H:i:s'));
 
                     DB::commit();
                     
