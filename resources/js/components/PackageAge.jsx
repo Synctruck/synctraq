c47f4516a09d75fa153f6ee7569fd4dfe63ce3f7
Alvaro Granillo <alvaro.granillo@inlandlogistics.co>
2023-12-11 15:30:24 -0600

    .

--- resources/js/components/PackageAge.jsx
+++ resources/js/components/PackageAge.jsx
@@ -281,7 +281,6 @@ function PackageAge() {
                                                     <option value="Failed">Failed</option>
                                                     <option value="NMI">NMI</option>
                                                     <option value="Middle Mile Scan">Middle Mile Scan</option>
-                                                    <option value="LM Carrier">LM Carrier</option>
                                                 </select>
                                             </div>
                                         </div>
