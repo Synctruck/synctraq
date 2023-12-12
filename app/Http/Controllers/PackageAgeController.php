ff92cff6f55ac9d84bd11ff2c21bc15bd8e1a8ab
Alvaro Granillo <alvaro.granillo@inlandlogistics.co>
2023-12-12 09:09:47 -0600

    LM Carrier

--- app/Http/Controllers/PackageAgeController.php
+++ app/Http/Controllers/PackageAgeController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\{ AuxDispatchUser, Comment, Configuration, Driver, 
                 PackageHistory, PackageBlocked, PackageDispatch, PackageFailed, PackageLost, 
                 PackageInbound, PackageManifest, PackageNeedMoreInformation, PackageNotExists, 
-                PackageReturn, PackageReturnCompany, PackageWarehouse, TeamRoute, User };
+                PackageReturn, PackageReturnCompany, PackageWarehouse, PackageLmCarrier, TeamRoute, User };
 
 use Illuminate\Support\Facades\Validator;
 
@@ -102,7 +102,7 @@ class PackageAgeController extends Controller
             $idsPackageDispatch  = PackageDispatch::where('status', '!=', 'Delivery')->get('Reference_Number_1');
             $idsPackageFailed    = PackageFailed::get('Reference_Number_1');
             $idsPackageNMI       = PackageNeedMoreInformation::get('Reference_Number_1');
-            $idsPackageLmCarrier = PackageWarehouse::get('Reference_Number_1');
+            $idsPackageLmCarrier = PackageLmCarrier::get('Reference_Number_1');
 
             $idsAll = $idsPackageInbound->merge($idsPackageWarehouse)->merge($idsPackageDispatch)->merge($idsPackageFailed)->merge($idsPackageNMI);
         }
@@ -140,7 +140,7 @@ class PackageAgeController extends Controller
         }
         else if($status == 'LM Carrier')
         {
-            $idsAll = PackageWarehouse::where('status', '=', 'LM Carrier')->get('Reference_Number_1');
+            $idsAll = PackageLmCarrier::where('status', '=', 'LM Carrier')->get('Reference_Number_1');
         }
 
         $states = $states == 'all' ? [] : explode(',', $states);
@@ -243,7 +243,6 @@ class PackageAgeController extends Controller
         $package = $package != null ? $package : PackageWarehouse::where('status', '=', 'Middle Mile Scan')->find($Reference_Number_1);
         $package = $package != null ? $package : PackageDispatch::where('status', '=', 'Dispatch')->find($Reference_Number_1);
         $package = $package != null ? $package : PackageDispatch::where('status', '=', 'Delete')->find($Reference_Number_1);
-        $package = $package != null ? $package : PackageWarehouse::where('status', '=', 'LM Carrier')->find($Reference_Number_1);
         $package = $package != null ? $package : PackageFailed::find($Reference_Number_1);
         $package = $package != null ? $package : PackageNeedMoreInformation::find($Reference_Number_1);
         $package = $package != null ? $package : PackageLost::find($Reference_Number_1);
