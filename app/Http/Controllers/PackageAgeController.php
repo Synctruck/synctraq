c97bbc4007393f2e41995b67b178fddc6e27e1aa
Alvaro Granillo <alvaro.granillo@inlandlogistics.co>
2023-12-14 15:30:50 -0600

    .

--- app/Http/Controllers/PackageAgeController.php
+++ app/Http/Controllers/PackageAgeController.php
@@ -102,7 +102,7 @@ class PackageAgeController extends Controller
             $idsPackageDispatch  = PackageDispatch::where('status', '!=', 'Delivery')->get('Reference_Number_1');
             $idsPackageFailed    = PackageFailed::get('Reference_Number_1');
             $idsPackageNMI       = PackageNeedMoreInformation::get('Reference_Number_1');
-            $idsPackageLmCarrier = PackageLmCarrier::where('status', '=', 'LM Carrier')->get('Reference_Number_1');
+            $idsPackageLmCarrier = PackageLmCarrier::where('status', '!=', 'Delivery')->get('Reference_Number_1');
 
             $idsAll = $idsPackageInbound->merge($idsPackageWarehouse)->merge($idsPackageDispatch)->merge($idsPackageFailed)->merge($idsPackageNMI)->merge($idsPackageLmCarrier);
         }
