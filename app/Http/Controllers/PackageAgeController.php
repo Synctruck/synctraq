7a803aaa40379125c99e6d96ac0b7a31abf164ff
Alvaro Granillo <alvaro.granillo@inlandlogistics.co>
2023-12-14 16:12:04 -0600

    .

--- app/Http/Controllers/PackageAgeController.php
+++ app/Http/Controllers/PackageAgeController.php
@@ -102,7 +102,7 @@ class PackageAgeController extends Controller
             $idsPackageDispatch  = PackageDispatch::where('status', '!=', 'Delivery')->get('Reference_Number_1');
             $idsPackageFailed    = PackageFailed::get('Reference_Number_1');
             $idsPackageNMI       = PackageNeedMoreInformation::get('Reference_Number_1');
-            $idsAll = PackageLmCarrier::where('status', '=', 'LM Carrier')
+            $idsPackageLmCarrier = PackageLmCarrier::where('status', '=', 'LM Carrier')
                              ->whereNotIn('Reference_Number_1', function($query) {
                                  $query->select('Reference_Number_1')
                                        ->from('PackageHistory')
