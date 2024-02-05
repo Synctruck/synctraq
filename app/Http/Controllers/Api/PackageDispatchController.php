546c2e65d495628e4a98996c13ce0e145254b445
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2024-02-05 17:12:26 -0500

    Update PackageDispatchController.php

--- app/Http/Controllers/Api/PackageDispatchController.php
+++ app/Http/Controllers/Api/PackageDispatchController.php
@@ -169,7 +169,7 @@ class PackageDispatchController extends Controller
                         {
                             $created_at = date('Y-m-d H:i:s');
 
-                            if($package->status == 'Manifest' || $package->status == 'Warehouse')
+                            if($package->status == 'Manifest' || $package->status == 'Inbound' || $package->status == 'Warehouse')
                             {
                                 $packageDispatch = new PackageDispatch();
                                 $packageDispatch->Reference_Number_1           = $package->Reference_Number_1;
