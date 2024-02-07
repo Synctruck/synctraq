14bfa532db21378313127f98cf2fdd6897125d0d
Alvaro Granillo <alvaro.granillo@inlandlogistics.co>
2024-02-07 10:36:50 -0600

    initDate

--- app/Console/Commands/TaskPackageSendPreFactura.php
+++ app/Console/Commands/TaskPackageSendPreFactura.php
@@ -94,12 +94,8 @@ class TaskPackageSendPreFactura extends Command
                 $files     = [];
                 $nowDate   = date('Y-m-d');
                 $startDate = date('2023-01-01');
-<<<<<<< Updated upstream
-                $endDate   = date('Y-m-d', strtotime($nowDate .' -3 day'));
-=======
                 $endDate   = date('Y-m-d', strtotime($nowDate .' -2 day'));
                 $initDate  = date('Y-m-d', strtotime($nowDate .' -8 day'));
->>>>>>> Stashed changes
 
                 $companyList = Company::all();
 
