69767cd5eb595b5c936b96fce1d1e45a0d77399b
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2024-02-09 10:55:42 -0500

    Update TaskPaymentTeam.php

--- app/Console/Commands/TaskPaymentTeam.php
+++ app/Console/Commands/TaskPaymentTeam.php
@@ -53,15 +53,14 @@ class TaskPaymentTeam extends Command
         $dayName = date("l");
         $nowHour = date('H');
 
-        if($nowHour > 9)
+        if($dayName == 'Monday' && $nowHour == 10)
         {
             $files     = [];
-            $nowDate   = date('Y-m-11');
+            $nowDate   = date('Y-m-d');
             $startDate = date('2023-11-01');
             $endDate   = date('Y-m-d', strtotime($nowDate .' -2 day'));
             $initDate   = date('Y-m-d', strtotime($nowDate .' -8 day'));
 
-            Log::info($nowHour);
             try
             {
                 DB::beginTransaction();
