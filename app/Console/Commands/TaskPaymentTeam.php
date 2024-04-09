17e406e31797791a09f50dcb8ae031e1fed98040
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2024-04-09 12:38:57 -0500

    ...

--- app/Console/Commands/TaskPaymentTeam.php
+++ app/Console/Commands/TaskPaymentTeam.php
@@ -53,16 +53,13 @@ class TaskPaymentTeam extends Command
         $dayName = date("l");
         $nowHour = date('H');
 
-        if($dayName == 'Tuesday')
+        if($dayName == 'Monday' && $nowHour == 10)
         {
             $files     = [];
             $nowDate   = date('Y-m-d');
-            //$startDate = date('2023-11-01');
-            //$endDate   = date('Y-m-d', strtotime($nowDate .' -2 day'));
-            $initDate   = date('Y-m-d', strtotime($nowDate .' -8 day'));
-
-            $startDate = date('2024-01-01');
-            $endDate = date('2024-12-31');
+            $startDate = date('2023-01-01');
+            $endDate   = date('Y-m-d', strtotime($nowDate .' -2 day'));
+            $initDate  = date('Y-m-d', strtotime($nowDate .' -8 day'));
 
             try
             {
