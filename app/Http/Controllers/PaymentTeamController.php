7262fcdf0bc1534d3b2d46890f78170b3b914391
Alvaro Granillo <alvaro.granillo@inlandlogistics.co>
2024-04-22 03:57:35 -0600

    hours

--- app/Http/Controllers/PaymentTeamController.php
+++ app/Http/Controllers/PaymentTeamController.php
@@ -972,7 +972,7 @@ class PaymentTeamController extends Controller
         $deduction = 0.00;
 
         if(in_array($packageRoute, $slaRoutes))
-            $deduction = $hours < 28 ? $sla_Deduction : 0.00;
+            $deduction = $hours > 28 ? $sla_Deduction : 0.00;
 
         return $deduction;
     }
