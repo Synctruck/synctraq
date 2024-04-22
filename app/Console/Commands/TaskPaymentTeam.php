5e645e184d2595c7e8a1ae5dbdfd13c92040dc77
Alvaro Granillo <alvaro.granillo@inlandlogistics.co>
2024-04-22 10:39:13 -0600

    >=

--- app/Console/Commands/TaskPaymentTeam.php
+++ app/Console/Commands/TaskPaymentTeam.php
@@ -53,7 +53,7 @@ class TaskPaymentTeam extends Command
         $dayName = date("l");
         $nowHour = date('H');
 
-        if($dayName == 'Monday' && $nowHour > 5)
+        if($dayName == 'Monday' && $nowHour >= 5)
         {
             $files     = [];
             $nowDate   = date('Y-m-d');
