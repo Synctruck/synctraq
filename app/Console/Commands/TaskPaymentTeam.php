3d12e12990db98d243a0b086e723ee828cd7a143
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2024-04-24 13:33:09 -0500

    Update TaskPaymentTeam.php

--- app/Console/Commands/TaskPaymentTeam.php
+++ app/Console/Commands/TaskPaymentTeam.php
@@ -58,7 +58,7 @@ class TaskPaymentTeam extends Command
             $files     = [];
             $nowDate   = date('Y-m-d');
             $startDate = date('2023-11-01');
-            $endDate   = date('Y-m-d', strtotime($nowDate .' -2 day'));
+            $endDate   = date('Y-m-d', strtotime($nowDate .' +2 day'));
             $initDate  = date('Y-m-d', strtotime($nowDate .' -8 day'));
 
             try
