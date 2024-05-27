72249d51f72f0bf571d05563696ea788938482ff
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2024-05-27 10:33:10 -0500

    Update TaskPackageSendPreFactura.php

--- app/Console/Commands/TaskPackageSendPreFactura.php
+++ app/Console/Commands/TaskPackageSendPreFactura.php
@@ -94,8 +94,8 @@ class TaskPackageSendPreFactura extends Command
                 $files     = [];
                 $nowDate   = date('Y-m-d');
                 $startDate = date('2023-01-01');
-                $endDate   = date('Y-m-d', strtotime($nowDate .' -4 day'));
-                $initDate  = date('Y-m-d', strtotime($nowDate .' -10 day'));
+                $endDate   = date('Y-m-d', strtotime($nowDate .' -2 day'));
+                $initDate  = date('Y-m-d', strtotime($nowDate .' -8 day'));
 
                 $companyList = Company::all();
 
