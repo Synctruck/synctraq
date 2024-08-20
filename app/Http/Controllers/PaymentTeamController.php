34b842a5bd5c0dd320f253480b03dfd1b2ce4387
wilber cahuana moreano <wilber.cahuana@synctruck.com>
2024-08-20 11:38:56 -0500

    Update PaymentTeamController.php

--- app/Http/Controllers/PaymentTeamController.php
+++ app/Http/Controllers/PaymentTeamController.php
@@ -1054,13 +1054,6 @@ class PaymentTeamController extends Controller
             $slaRoutes = array_map('trim', $slaRoutes);
 
             Log::info("hours:". $hours);
-            Log::info("hours:". $hours);
-            Log::info("hours:". $hours);
-            Log::info("hours:". $hours);
-            Log::info("hours:". $hours);
-            Log::info("hours:". $hours);
-            Log::info("hours:". $hours);
-            Log::info("hours:". $hours);
             Log::info("Date_Dispatch:". $Date_Dispatch);
             Log::info("Date_Delivery:". $Date_Delivery);
 
