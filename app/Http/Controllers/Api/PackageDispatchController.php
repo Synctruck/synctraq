9e10fd6972a5beec5e7e93afedcd8f85a5a88e86
Alvaro Granillo <alvaro.granillo@inlandlogistics.co>
2024-09-04 16:57:32 -0600

    findRts

--- app/Http/Controllers/Api/PackageDispatchController.php
+++ app/Http/Controllers/Api/PackageDispatchController.php
@@ -233,6 +233,8 @@ class PackageDispatchController extends Controller
         $package = $package ? $package : PackageLmCarrier::where('status', 'LM Carrier')->find($request['barcode']);
         $package = $package ? $package : PackageDispatch::where('status', 'Delivery')->find($request['barcode']);
         $package = $package ? $package : PackageNeedMoreInformation::where('status', 'NMI')->find($request['barcode']);
+        $package = $package ? $package : PackageReturnCompany::where('status', 'ReturnCompany')->find($request['barcode']);
+        $package = $package ? $package : PackageReturnCompany::where('status', 'PreRts')->find($request['barcode']);
         Log::info($package);
         if($package)
         {
