072068b565ccf9e92b0e1a57073f77d66c0d8694
Alvaro Granillo <alvaro.granillo@inlandlogistics.co>
2024-10-18 11:08:32 -0600

    user syncfreight

--- resources/js/components/Login.js
+++ resources/js/components/Login.js
@@ -80,17 +80,19 @@ function Login() {
 
               <div className="d-flex justify-content-center py-4">
                   <img  src="/img/logo_sync_truck.png" width={200} alt="img"/>
+                  <h3>Please use Syncfreight to do this process.</h3>
+                                                <p>
+                                                    <a href="https://platform.syncfreight.com" target="_blank" rel="noopener noreferrer">
+                                                        https://platform.syncfreight.com
+                                                    </a>
+                                                </p>
               </div>
 
                   <div className="card mb-3">
-                  <div className="pt-4 pb-2">
-                        <h5 className="card-title text-center pb-0 fs-4">Login</h5>
-                        <p className="text-center small">Enter your email and password</p>
-                      </div>
 
                     <div className="card-body" style={{ display: 'none' }}>
 
-                      <div className="pt-4 pb-2">
+                      <div className="pt-4 pb-2" style={{ display: 'none' }}>
                         <h5 className="card-title text-center pb-0 fs-4">Login</h5>
                         <p className="text-center small">Enter your email and password</p>
                       </div>
