fa138af9c6165f3e3e7745e9be4190fc54cc9067
Alvaro Granillo <alvaro.granillo@inlandlogistics.co>
2024-10-18 11:02:50 -0600

    update

--- resources/js/components/Login.js
+++ resources/js/components/Login.js
@@ -86,7 +86,7 @@ function Login() {
 
                     <div className="card-body" style={{ display: 'none' }}>
 
-                      <div className="pt-4 pb-2" style={{ display: 'none' }}>
+                      <div className="pt-4 pb-2">
                         <h5 className="card-title text-center pb-0 fs-4">Login</h5>
                         <p className="text-center small">Enter your email and password</p>
                       </div>
