c84a6a8712857a6018296783a461adaec641ba13
Alvaro Granillo <alvaro.granillo@inlandlogistics.co>
2024-10-18 10:58:43 -0600

    login disable

--- resources/js/components/Login.js
+++ resources/js/components/Login.js
@@ -86,7 +86,7 @@ function Login() {
 
                     <div className="card-body">
 
-                      <div className="pt-4 pb-2">
+                      <div className="pt-4 pb-2" style={{ display: 'none' }}>
                         <h5 className="card-title text-center pb-0 fs-4">Login</h5>
                         <p className="text-center small">Enter your email and password</p>
                       </div>
